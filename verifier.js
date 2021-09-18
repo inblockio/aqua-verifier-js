efd9af913ef17f1d80892ba917240fc3a3ba5c77
rht <rhtbot@protonmail.com>
2021-09-18 03:32:00 -0400

    CLI: Allow user to specify server

--- verifier.js
+++ verifier.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 
+const argv = require('minimist')(process.argv.slice(2));
 const main = require('./index')
 
 //This should be a commandline argument for specifying the title of the page which should be verified 
-if (process.argv.length < 3) {
+if (argv._.length < 1) {
   main.log_red("ERROR: You must specify the page title")
   process.exit(1)
 }
-let title = process.argv[2] !== '-v' ? process.argv[2]: process.argv[3]
+const title = argv._[0]
 
-VERBOSE = process.argv.includes('-v')
+const verbose = argv.v
+
+const server = argv.server ?? 'http://localhost:9352'
 
 console.log(`Verifying ${title}`)
-main.verifyPage(title, 'http://localhost:9352', VERBOSE)
+main.verifyPage(title, server, verbose)
