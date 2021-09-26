832762535fbdec481fb1db8025e1e1412e333288
rht <rhtbot@protonmail.com>
2021-09-26 04:19:09 -0400

    Add usage to verifier.js

--- verifier.js
+++ verifier.js
@@ -3,9 +3,20 @@
 const argv = require('minimist')(process.argv.slice(2));
 const main = require('./index')
 
+function usage() {
+  console.log(`Usage:
+verifier.js [OPTIONS] <page title>
+
+Options:
+  -v       verbose
+  --server <The url of the server, e.g. https://pkc.inblock.io>
+If the --server is not specified, it defaults to http://localhost:9352`)
+}
+
 //This should be a commandline argument for specifying the title of the page which should be verified 
 if (argv._.length < 1) {
   main.log_red("ERROR: You must specify the page title")
+  usage()
   process.exit(1)
 }
 const title = argv._[0]
