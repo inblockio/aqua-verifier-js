6472772599dc424568301b0be4fb8581993145bb
rht <rhtbot@protonmail.com>
2021-10-25 03:10:25 -0400

    Add docstring to checkEtherScan()

--- checkEtherScan.js
+++ checkEtherScan.js
@@ -16,6 +16,22 @@ function sleep(ms) {
   });
 }
 
+/**
+ * Checks if the witness verification hash matches the hash timestamped on an
+ * Ethereum blockchain, via etherscan.io.
+ * Steps:
+ * - Determines the etherscan.io witness url from the witness network passed
+ *   in.
+ * - Does a lookup of a hash from the witness url combined with the transaction
+ *   hash.
+ * - Returns true if witnessVerificationHash equals the hash value returned
+ *   from the previous step, false otherwise.
+ * @param {string} witnessNetwork see the keys of witnessNetworkMap for
+ *     possible values.
+ * @param {string} txHash the Ethereum transaction hash.
+ * @param {string} witnessVerificationHash SHA3 verification hash
+ * @returns {boolean} whether the hash in the lookup matches witnessVerificationHash
+ */
 async function checkEtherScan(witnessNetwork, txHash, witnessVerificationHash) {
   try {
     promise = new Promise((resolve, reject) => {
