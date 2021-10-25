cba257441ca10335daa54497b643bc1eb261d239
BennSim <92349227+BennSim@users.noreply.github.com>
2021-10-25 14:39:41 +0800

    Update checkEtherScan.js

--- checkEtherScan.js
+++ checkEtherScan.js
@@ -16,7 +16,8 @@ function sleep(ms) {
   });
 }
 
-/** function checkEtherScan is passed witness network, transaction hash, verification hash as parameters.
+/** 
+  * function checkEtherScan is passed witness network, transaction hash, verification hash as parameters.
   * Notes on the logic:
   * Determines witness url from the witness network passed in.
   * Does a lookup with the witness url and transaction hash (Ethereum blockchain).
